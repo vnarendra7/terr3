57c16401447625ec8061e90f2a12f7361c49fce6
vnarendra7 <109133334+vnarendra7@users.noreply.github.com>
2022-09-09 16:53:50 +0530

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -3,4 +3,4 @@ sudo yum update
 sudo yum install -y httpd
 sudo chkconfig httpd on
 sudo service httpd start
-echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Yes, Narendra You have deployed via terraform</h1>" | sudo tee /var/www/html/index.html
